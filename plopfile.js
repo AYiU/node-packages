ac716627cedbd4047759595a5e442d311b3bbf9e
AYiU <ayiu@ayiu.net>
2022-08-09 22:41:48 +0800

    Init commit with pnpm workspace and plop

---
+++ plopfile.js
@@ -0,0 +1,30 @@
+export default function (plop) {
+  // create your generators here
+  plop.setGenerator("controller", {
+    description: "application controller logic",
+    prompts: [
+      {
+        type: "input",
+        name: "packageName",
+        message: "Package name please (file-name)",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "packages/{{packageName}}/{{packageName}}.ts",
+        templateFile: "plop-templates/package/package-name.ts",
+      },
+      {
+        type: "add",
+        path: "packages/{{packageName}}/package.json",
+        templateFile: "plop-templates/package/package.json.hbs",
+      },
+      {
+        type: "add",
+        path: "packages/{{packageName}}/tsconfig.json",
+        templateFile: "plop-templates/package/tsconfig.json",
+      },
+    ],
+  });
+}
