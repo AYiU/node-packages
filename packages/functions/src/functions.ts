9972a3f154dac9667f9f930ea58b412dc15f2366
AYiU <ayiu@ayiu.net>
2022-08-11 00:54:22 +0800

    Added: functions package.

---
+++ packages/functions/src/functions.ts
@@ -0,0 +1,9 @@
+import crypto from "crypto";
+
+export function sha1(string: crypto.BinaryLike) {
+  return crypto.createHash("sha1").update(string).digest("hex");
+}
+
+export function md5(string: crypto.BinaryLike) {
+  return crypto.createHash("md5").update(string).digest("hex");
+}
