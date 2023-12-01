441b1f44ef982ae19aceb50b12a160e88af348f7
AYiU <ayiu@ayiu.net>
2023-12-01 18:53:32 +0800

    Updated: type, readme, dependency for dayjs package

--- packages/dayjs/README.md
+++ packages/dayjs/README.md
@@ -1,3 +1,37 @@
-DayJs
+# DayJs
 
-Helper function for dayjs.
+Plugin for dayjs.
+
+## Install
+
+```
+import dayjs from "dayjs";
+import Plugin from "@yiuayiu/dayjs";
+
+dayjs.extend(Plugin);
+
+```
+
+## Functions
+
+```
+dayjs().formatDate(defaultValue?: string): string;
+```
+
+If date is valid, return format date YYYY-MM-DD HH:mm, else return defaultValue.
+
+```
+dayjs().fromNowX(defaultValue?: string): string;
+```
+
+If date is valid, return fromNow() function, else return defaultValue.
+
+```
+dayjs().displayDateOrTime(): string;
+```
+
+If current time - value in dayjs is > 24 hours, return format date YYYY-MM-DD, else return format HH:mm.
+
+```
+
+```
--- packages/dayjs/src/dayjs.ts
+++ packages/dayjs/src/dayjs.ts
@@ -1,4 +1,4 @@
-import dayjs, { PluginFunc } from "dayjs";
+import dayjs, { PluginFunc, ConfigType, Dayjs } from "dayjs";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
 import relativeTime from "dayjs/plugin/relativeTime";
@@ -9,10 +9,24 @@ dayjs.extend(relativeTime);
 
 declare module "dayjs" {
   interface Dayjs {
+    // Copy from timezone plugin
+    tz(timezone?: string, keepLocalTime?: boolean): dayjs.Dayjs;
+    offsetName(type?: "short" | "long"): string | undefined;
+
     formatDate(defaultValue?: string): string;
     fromNowX(defaultValue?: string): string;
     displayDateOrTime(): string;
   }
+
+  // Copy from timezone plugin
+  interface DayjsTimezone {
+    (date?: dayjs.ConfigType, timezone?: string): dayjs.Dayjs;
+    (date: dayjs.ConfigType, format: string, timezone?: string): dayjs.Dayjs;
+    guess(): string;
+    setDefault(timezone?: string): void;
+  }
+
+  // const tz: DayjsTimezone;
 }
 
 const plugin: PluginFunc = (option, dayjsClass, dayjsFactory) => {
