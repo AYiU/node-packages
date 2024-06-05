3a34a7aeda20196fcacad8513d11806c98a9a0e4
AYiU <ayiu@ayiu.net>
2024-06-05 13:48:33 +0800

    Updated: package and test in functions

---
+++ packages/functions/src/functions.test.ts
@@ -0,0 +1,9 @@
+import { md5, sha1 } from "./functions.js";
+
+test("sha1", () => {
+  expect(sha1("hello")).toBe("aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d");
+});
+
+test("md5", () => {
+  expect(md5("hello")).toBe("5d41402abc4b2a76b9719d911017c592");
+});
